b818c932e002e512d8fc5290d5cb20c564b4d8a5
Steven Pav <shabbychef@gmail.com>
2024-11-02 18:36:16 -0700

    adding proper reference to arxiv paper

--- man-roxygen/ref-pav.R
+++ man-roxygen/ref-pav.R
@@ -1,5 +1,5 @@
 #' @references
 #'
 #' Pav, S. E. "An Iterative Algorithm for Regularized Non-negative Matrix Factorizations."
-#' Forthcoming. (2024)
+#' Arxiv pre-print (2024).  \doi{10.48550/arXiv.2410.22698}
 #'
